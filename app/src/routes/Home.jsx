452758925b3f2046ee303950210f52e375ea424a
naiguaraldo <95829961+naiguaraldo@users.noreply.github.com>
2021-12-09 23:48:08 -0300

    Início de formulário usando bootstrap

--- app/src/routes/Home.jsx
+++ app/src/routes/Home.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Container, Row, Form, FormLabel, InputGroup } from 'react-bootstrap';
 
 const Home = () => {
     return (
         <>
-            <button>botão</button>
-            <div>Teste</div>
             <Container>
                 <Row>
-                    <Col>
-                        <Button>Button com bootstrap</Button>
-                    </Col>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Nome:</Form.Label>
+                            <Form.Control type="text" placeholder="Digite seu nome..." />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Label>E-mail:</Form.Label>
+                            <Form.Control type="email" placeholder="Digite seu e-mail..." />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <FormLabel> CPF:</FormLabel>
+                            <Form.Control type="text" placeholder="Digite seu CPF..." />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <InputGroup className="mb-3">
+                                <InputGroup.Checkbox />
+                                <span class="d-flex align-items-center px-2">Declaro que li os termos e condições.</span>
+                            </InputGroup>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Button variant="primary">Enviar</Button>
+                        </Form.Group>
+                    </Form>
                 </Row>
             </Container>
         </>
